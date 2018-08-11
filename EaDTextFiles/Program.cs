9dd0b50bcf10f83339d6f4a5d9a340806ea5bcaa
Michel Zuidema <michelgzuidema@gmail.com>
2018-08-11 02:30:39 +0200

    Added: Checked if a txt file was dragged, added Encrypt and Decrypt functions, Encrypt function not working yet.

--- EaDTextFiles/Program.cs
+++ EaDTextFiles/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EncryptAndDecrypt;
+using System.IO;
 
 namespace EaDTextFiles
 {
@@ -10,6 +12,84 @@ namespace EaDTextFiles
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please select what you would like to do:\n1: Encrypt Text File\n2: Decrypt Text\n");
+            string UserChoice = Console.ReadLine();
+            if(UserChoice == "1")
+            {
+                if (args.Length > 0)
+                {
+                    string FilePath = args[0];
+
+                    if (File.Exists(FilePath))
+                    {
+                        using (StreamReader sr = new StreamReader(FilePath))
+                        {
+                            string FileContent = sr.ReadToEnd();
+                            EncryptFileContent(FileContent, FilePath);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Filepath not found.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter the filepath of the text file: ");
+                    string FilePath = Console.ReadLine();
+                    if (FilePath.Length > 0)
+                    {
+                        if (File.Exists(FilePath))
+                        {
+                            using (StreamReader sr = new StreamReader(FilePath + "Encrypted.txt"))
+                            {
+                                string FileContent = sr.ReadToEnd();
+                                EncryptFileContent(FileContent, FilePath);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("File Not Found.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No FilePath Entered.");
+                    }
+                }
+            } else
+            {
+
+            }
+            ExitProgram();
+        }
+
+        static void EncryptFileContent(string FileContent, string FilePath)
+        {
+            if(FileContent.Length > 0)
+            {
+                string EncryptedText = EncryptAndDecrypt.EncryptDecrypt.Encrypt(FileContent);
+
+                using(StreamWriter sw = new StreamWriter(FilePath))
+                {
+                    sw.WriteLine(EncryptedText);
+                    Console.WriteLine("Encrypted text added to FilePath");
+                }
+            } else
+            {
+                Console.WriteLine("No File Content");
+            }
+        }
+
+        static void DecryptFileContent(string FileContent, string FilePath)
+        {
+
+        }
+
+        static void ExitProgram()
+        {
+            Console.WriteLine("Press ENTER to exit...");
+            Console.ReadLine();
         }
     }
 }
